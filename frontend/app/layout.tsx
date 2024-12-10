2cb2ab1ef2c218a5b553662172ed79ce74716344
Loay Waleed <loaywaleed9@gmail.com>
2024-12-10 21:04:57 +0200

    Project setup

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,25 @@
+import '@mantine/core/styles.css';
+
+import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
+
+export const metadata = {
+  title: 'Financial Instruments',
+  description: 'a simple financial instruments app',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
