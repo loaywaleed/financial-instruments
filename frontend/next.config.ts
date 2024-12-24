61b8bd8b0ab99675416f8e33ebff7784425db748
Loay Waleed <loaywaleed9@gmail.com>
2024-12-24 21:34:57 +0200

    Added dockerfile

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: "http://localhost:8000/api/v1/:path*",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png|css|js|json)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
